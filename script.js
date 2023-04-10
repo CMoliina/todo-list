1a7e8cf2cd7235b242c9a2c26b131f586f89b37b
Camila Molina <molina.neculnir@gmail.com>
2023-04-09 21:12:03 -0400

    functions created

--- script.js
+++ script.js
@@ -1,32 +1,67 @@
-const date = document.querySelector('#date');
-const list = document.querySelector('#list');
-const input = document.querySelector('#input');
-const enterButton = document.querySelector('#enter');
+const date = document.querySelector('#date')
+const list = document.querySelector('#list')
+const input = document.querySelector('#input')
+const enterButton = document.querySelector('#enter')
+const check = 'fa-check-circle'
+const uncheck = 'fa-circle'
+const lineThrough = 'line-through'
+let id = 0
 
 // Add task function
-function addTask(task) {
+function addTask(task, id, done, deleted) {
+    if(deleted){
+        return
+    }
+    const doneTask = done ? check : uncheck 
+    const line = done ? lineThrough : ''
     const element = `<li id="element">
-                    <i class="far fa-circle co" data="done" id="0"></i>
-                    <p class="text">${task}</p>
-                     <i class="fas fa-trash de" data="deleted" id="0"></i>
+                    <i class="far ${doneTask}" data="done" id="${id}"></i>
+                    <p class="text" ${line}>${task}</p>
+                     <i class="fas fa-trash de" data="deleted" id="${id}"></i>
                     </li>`
     list.insertAdjacentHTML("beforeend", element)
+}
+
+// Done task function
+function TaskDone(element){
+    element.classList.toggle(check)
+    element.classList.toggle(uncheck)
+    element.parentNode.querySelector('.text').classList.toggle(lineThrough)
+}
+
+// Deleted task function
+function TaskDeleted(element){
+    element.parentNode.parentNode.removeChild(element.parentNode)
 }
 
 enterButton.addEventListener('click', () => {
     const task = input.value
     if(task){
-        addTask(task)
+        addTask(task, id, false, false)
     }
     input.value = ''
+    id++
 })
 
 document.addEventListener('keyup', function(event){
     if(event.key == 'Enter'){
         const task = input.value
         if(task){
-            addTask(task)
+            addTask(task, id, false, false)
         }
         input.value = ''
+        id++
+    }
+})
+
+list.addEventListener('click', function(event){
+    const element = event.target
+    const elementData = element.attributes.data.value
+   
+    if(elementData === 'done'){
+        TaskDone(element)
+    }
+    else if (elementData === 'deleted'){
+        TaskDeleted(element)
     }
 })
